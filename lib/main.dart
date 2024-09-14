9ef4f3338c287087ee0af0fb9aa6119e26ab022a
turki-aloufi <turkialoufi44@gmail.com>
2024-09-15 00:09:47 +0300

    create my projects ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:project7/layers/auth_layer.dart';
 import 'package:project7/models/auth_model.dart';
 import 'package:project7/screens/login/login_screen.dart';
+import 'package:project7/screens/my_projects/my_projects_screen.dart';
 import 'package:project7/screens/navigation/navigation_screen.dart';
 import 'package:project7/services/setup.dart';
 
@@ -18,6 +19,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AuthModel? auth = GetIt.I.get<AuthLayer>().auth;
-    return MaterialApp(home: auth==null ? const LoginScreen() : const NavigationScreen());
+    // return MaterialApp(home: auth==null ? const LoginScreen() : const NavigationScreen());
+    return const MaterialApp(home: MyProjectsScreen());
   }
 }
